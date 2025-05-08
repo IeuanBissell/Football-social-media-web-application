6a3ab9835b29018288e68a806e4dc1aec38a6a83
Ieuan Bissell <ieuan.e.bissell@gmail.com>
2025-05-08 19:56:29 +0100

    Removed debugging

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,23 +19,8 @@ class DashboardController extends Controller
         $user = Auth::user();
 
         // Get all notifications for display, not just unread ones
-        // We don't need to query this separately since the logged-in user already has the notifications relation
-        // This is just for clarity
         $notifications = $user->notifications()->latest()->limit(5)->get();
 
-        // Debug: Log notification details
-        Log::info('User notifications', [
-            'user_id' => $user->id,
-            'notification_count' => count($notifications),
-            'notification_details' => $notifications->map(function($n) {
-                return [
-                    'id' => $n->id,
-                    'type' => $n->type,
-                    'read_at' => $n->read_at,
-                    'data' => $n->data,
-                ];
-            })
-        ]);
 
         return view('dashboard', compact('notifications'));
     }
